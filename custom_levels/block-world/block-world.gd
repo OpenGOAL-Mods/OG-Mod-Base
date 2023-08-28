c125159e0368fa38ecc0111cdfab684215aeea95
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2023-08-28 16:33:26 -0400

    add chicken to portal

--- custom_levels/block-world/block-world.gd
+++ custom_levels/block-world/block-world.gd
@@ -3,9 +3,9 @@
 "block-world.go"
 "block-world-code.o"
 ;;Put back at some point once crash is figured out
-;; "warp-gate-switch-ag.go"
-;; "training-part.o"
-;; "warpgate-ag.go"
-;; "villagep-obs.o"
+"warp-gate-switch-ag.go"
+"training-part.o"
+"warpgate-ag.go"
+"villagep-obs.o"
 ))
 
