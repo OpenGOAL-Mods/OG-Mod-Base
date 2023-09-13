cd4cfc458284bf2f0638707c3166efb33f58c63e
JoKronk <JoKronkvist@gmail.com>
2023-09-13 12:47:25 +0300

    Add jak1 multitarget logic from old base

--- goal_src/jak1/dgos/game.gd
+++ goal_src/jak1/dgos/game.gd
@@ -31,6 +31,7 @@
   "dma-bucket.o"
   "dma-disasm.o"
   "pckernel-h.o" ;; added
+  "mod-settings-h.o" ;; added
   "pckernel-impl.o" ;; added
   "pc-debug-common.o" ;; added
   "pc-debug-methods.o" ;; added
