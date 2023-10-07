f2f41449aeb152abf1e688f98975c5e0bd44a9f8
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2023-10-07 04:23:58 -0400

    cleanup gd files

--- custom_levels/block-world/block-world.gd
+++ custom_levels/block-world/block-world.gd
@@ -4,21 +4,11 @@
 "block-world-code.o"
 ;;Put back at some point once crash is figured out
 "training-part.o"
-
 "warp-gate-switch-ag.go"
 "warpgate-ag.go"
-"wobbler.o"
-"twister.o"
 "flutflutegg.o"
 "warpgate.o"
-"lurkerworm.o"
-"misty-teetertotter.o"
-"teetertotter-ag.go"
-"lurkerworm-ag.go"
-"windmill-one-ag.go"
 "flutflut-ag.go"
 "flutflutegg-ag.go"
-
-
 ))
 
