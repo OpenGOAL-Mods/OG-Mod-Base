c39fc5dd23bb2e72149eb0257140421b074bd64d
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2023-10-25 20:20:43 -0400

    Fix custom level textures

--- goalc/build_level/common/gltf_mesh_extract.cpp
+++ goalc/build_level/common/gltf_mesh_extract.cpp
@@ -293,7 +293,7 @@ int texture_pool_debug_checker(TexturePool* pool) {
 
 int texture_pool_add_texture(TexturePool* pool, const tinygltf::Image& tex) {
   const auto& existing = pool->textures_by_name.find(tex.name);
-  if (existing != pool->textures_by_name.end()) {
+  if (2 == 3) {
     lg::info("Reusing image: {}", tex.name);
     return existing->second;
   } else {
