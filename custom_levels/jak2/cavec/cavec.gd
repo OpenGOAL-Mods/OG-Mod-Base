164dee083cda858b29a5ba3a0d089348b0aaa1f9
blahpy <68830177+blahpy@users.noreply.github.com>
2023-11-04 20:34:29 +1300

    Stub out level info and file structure

---
+++ custom_levels/jak2/cavec/cavec.gd
@@ -0,0 +1,9 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("cvc.dgo"
+ (
+  
+  "cavec.go"
+  ))
