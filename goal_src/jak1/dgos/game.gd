082458a53ec5684523f0aa2f828b87fbb44890f4
dallmeyer <2515356+dallmeyer@users.noreply.github.com>
2024-02-08 00:22:12 -0800

    init skeleton for orb placer

--- goal_src/jak1/dgos/game.gd
+++ goal_src/jak1/dgos/game.gd
@@ -348,6 +348,7 @@
   "hud-classes-pc.o" ;; added
   "mod-settings.o" ;; added
   "mod-common-functions.o" ;; added
+  "orb-placer.o" ;; added
   "mod-custom-code.o" ;; added
   ;; keep zoomer stuff loaded
   "target-racer-h.o"
