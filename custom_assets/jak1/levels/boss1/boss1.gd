b6d1e8cd21c552bc77bbacdf8b4d89c77e707233
blahpy <68830177+blahpy@users.noreply.github.com>
2024-06-14 21:18:42 +1200

    Moved files to custom_assets

---
+++ custom_assets/jak1/levels/boss1/boss1.gd
@@ -0,0 +1,15 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("B01.DGO"
+ ("static-screen.o"
+  "boss1-control-h.o"
+  "boss1-attack-h.o"
+  "boss1-attack.o"
+  "boss1-control.o"
+  "boss1-part.o"
+  "plat-ag.go"
+  "water-anim-robocave-ag.go"
+  "boss1.go"
+ ))
