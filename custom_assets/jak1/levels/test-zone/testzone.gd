bc2454ec3433b4a8a787028c3f47ca8581ae9a98
Grateful Forest 🌲 <168700820+Grateful-Forest@users.noreply.github.com>
2024-08-29 05:32:19 +0930

    Add files via upload

--- custom_assets/jak1/levels/test-zone/testzone.gd
+++ custom_assets/jak1/levels/test-zone/testzone.gd
@@ -1,12 +1,10 @@
-;; DGO definition file for Awful Village level
-;; We use the convention of having a longer DGO name for levels without precomputed visibility.
-
-;; the actual file name still needs to be 8.3
 ("TSZ.DGO"
- ("static-screen.o"
-  "test-zone-obs.o"
+  (
+  "static-screen.o"
   "plat-ag.go"
-  "test-actor-ag.go"
-  "babak-ag.go"
+  "plat-eco-ag.go"
+  "water-anim-village1-ag.go"
+  "test-zone-obs.o"
   "test-zone.go"
- ))
+  )
+)
